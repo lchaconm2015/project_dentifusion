ef1f980449ffbf18c21cb84cccd62be41c00e69c
lester <lchaconm2015@gmail.com>
2026-03-21 18:10:55 -0500

    [commit]- subiendo nuevos addons

--- df_dental_appointment/__manifest__.py
+++ df_dental_appointment/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "DF Dental Appointment",
     "version": "16.0.1.0.0",
     "summary": "Agenda y citas odontológicas con calendario Odoo",
-    "category": "Healthcare1",
+    "category": "Healthcare12",
     "author": "LICSYST",
     "website": "https://www.licsyst.com",
     "license": "LGPL-3",
