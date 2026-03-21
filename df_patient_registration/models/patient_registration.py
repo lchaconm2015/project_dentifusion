0c5bafa3a81dd50458115ae2b8f8d45283ba8ba0
lester <lchaconm2015@gmail.com>
2026-03-20 23:32:03 -0500

    [commit]- agrgando reporte de historia clinica

--- df_patient_registration/models/patient_registration.py
+++ df_patient_registration/models/patient_registration.py
@@ -319,3 +319,4 @@ class DfPatientRegistrationTreatmentLine(models.Model):
     diagnosis_complications = fields.Text(string="Diagnósticos y complicaciones")
     procedures = fields.Text(string="Procedimientos")
     prescriptions = fields.Text(string="Prescripciones")
+    cost = fields = fields.Text(string="Costo")
